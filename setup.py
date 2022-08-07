af3cedf3b234d25c65a24e600c4b218dd7e855cb
Vyacheslav Surkov <surkov.vo@phystech.edu>
2022-08-07 19:22:04 +0300

    setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bert_dp',
+    packages=find_packages(),
+    version=1.0,
+    description='JointGT adjusted to DeepPavlov',
+    author='surkovv, thu-coai',
+    author_email='surokpro2@gmail.com',
+    include_package_data=True,
+    license='Apache License, Version 2.0'
+)
